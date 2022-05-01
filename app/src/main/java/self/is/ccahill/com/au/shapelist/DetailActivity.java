e518edb4c5a3af88e4507cf87f719760a5416798
Matthew822 <57922178+matthew822-e@users.noreply.github.com>
2022-05-01 13:35:25 -0400

    CS321

---
+++ app/src/main/java/self/is/ccahill/com/au/shapelist/DetailActivity.java
@@ -0,0 +1,48 @@
+package self.is.ccahill.com.au.shapelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class DetailActivity extends AppCompatActivity
+{
+    Shape selectedShape;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        getSelectedShape();
+        setValues();
+
+    }
+
+    private void getSelectedShape()
+    {
+        Intent previousIntent = getIntent();
+        String parsedStringID = previousIntent.getStringExtra("id");
+        selectedShape = MainActivity.shapeList.get(Integer.valueOf(parsedStringID));
+    }
+
+    private void setValues()
+    {
+        TextView tv = (TextView) findViewById(R.id.shapeName);
+        ImageView iv = (ImageView) findViewById(R.id.shapeImage);
+
+        tv.setText(selectedShape.getName());
+        iv.setImageResource(selectedShape.getImage());
+    }
+}
